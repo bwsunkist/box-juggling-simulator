c41f23bdb03f9439ec6ed07fe053504e663c4704
bwsunkist <54732163+bwsunkist@users.noreply.github.com>
2022-07-30 00:37:24 +0900

    feat: add drag and drop for box

--- components/Views/JugglingView.vue
+++ components/Views/JugglingView.vue
@@ -55,32 +55,66 @@ const dragHand = (event, isRight: boolean) => {
   event.dataTransfer.dropEffect = 'move'
   event.dataTransfer.setData('dragHand', isRight ? 'rightHand' : 'leftHand')
 }
-const dropObject = (event, dropCategory) => {
+
+const dragBox = (event, boxIndex: number) => {
+  event.dataTransfer.effectAllowed = 'move'
+  event.dataTransfer.dropEffect = 'move'
+  event.dataTransfer.setData('dragBox', boxIndex)
+}
+
+const dropObject = (event, dropBoxIdx: number) => {
   const dragHandEvt = event.dataTransfer.getData('dragHand')
   const dragBoxEvt = event.dataTransfer.getData('dragBox')
-  const targetBox = dropCategory
 
   if (dragHandEvt) {
-    hangBox(dragHandEvt, targetBox)
+    hangBox(dragHandEvt, dropBoxIdx)
   } else if (dragBoxEvt) {
     // TODO
+    replaceBox(dragBoxEvt, dropBoxIdx)
   }
 }
 
-const hangBox = (dragHandEvt: string, targetBoxName: string) => {
-  boxes.forEach((box, index) => {
-    if (box.message !== targetBoxName || box.isHold) return
+const hangBox = (dragHandEvt: string, targetBoxIdx: number) => {
+  if (boxes[targetBoxIdx].isHold) return
 
-    boxes[index].isHold = true
-    if (dragHandEvt === 'rightHand') {
-      isShowRightHand.value = false
-      boxes[index].isRight = true
+  boxes[targetBoxIdx].isHold = true
+  if (dragHandEvt === 'rightHand') {
+    isShowRightHand.value = false
+    boxes[targetBoxIdx].isRight = true
+  }
+  if (dragHandEvt === 'leftHand') {
+    isShowLeftHand.value = false
+    boxes[targetBoxIdx].isRight = false
+  }
+}
+
+const replaceBox = (dragBoxEvt: number, targetBoxIdx: number) => {
+  const idxAbsDiff = Math.abs(dragBoxEvt - targetBoxIdx)
+  if (idxAbsDiff === 0 ) {
+    return
+  } else if (idxAbsDiff === 1) {
+    // 単純に入れ替える
+    const tmp = boxes[dragBoxEvt]
+    boxes[dragBoxEvt] = boxes[targetBoxIdx]
+    boxes[targetBoxIdx] = tmp
+  } else {
+    // 端から端へ移動する
+    const idxDiff = dragBoxEvt - targetBoxIdx
+    // TODO ベタ書きやめる
+    if (idxDiff > 0) {
+      // dragBoxEvt = 2, targetBoxIdx = 0 のとき
+      const tmp = boxes[2]
+      boxes[2] = boxes[1]
+      boxes[1] = boxes[0]
+      boxes[0] = tmp
+    } else {
+      // dragBoxEvt = 0, targetBoxIdx = 2 のとき
+      const tmp = boxes[0]
+      boxes[0] = boxes[1]
+      boxes[1] = boxes[2]
+      boxes[2] = tmp
     }
-    if (dragHandEvt === 'leftHand') {
-      isShowLeftHand.value = false
-      boxes[index].isRight = false
-    }
-  })
+  }
 }
 </script>
 
@@ -93,8 +127,8 @@ const hangBox = (dragHandEvt: string, targetBoxName: string) => {
       <Hand :isHold="false" :isRight="true" v-show="isShowRightHand" :draggable="true" @dragstart="dragHand($event, true)"></Hand>
     </div>
     <div class="boxWrap">
-      <div v-for="(box, index) in boxes" @drop="dropObject($event, `box${index + 1}`)" @dragover.prevent @dragenter.prevent>
-        <BoxWithHand :message="box.message" :isHold="box.isHold" :isRight="box.isRight" :rotate="box.rotate" :draggable="box.isHold"></BoxWithHand>
+      <div v-for="(box, index) in boxes" @drop="dropObject($event, index)" @dragover.prevent @dragenter.prevent>
+        <BoxWithHand :message="box.message" :isHold="box.isHold" :isRight="box.isRight" :rotate="box.rotate" :draggable="box.isHold" @dragstart="dragBox($event, index)"></BoxWithHand>
       </div>
     </div>
     <div class="operationArea">
